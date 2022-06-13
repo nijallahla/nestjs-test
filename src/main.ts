a17c1669622bc515ce8f83943066be62d9f8a81a
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-06-13 14:07:55 +0900

    Add CORS and x-powered-by

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Remove "X-Powered-By" header from response
+  app.disable('x-powered-by');
+
+  app.enableCors({
+    origin: 'http://example.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
